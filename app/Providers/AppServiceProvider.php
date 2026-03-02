c7b986444a7bf85092863184fcdbc3b4f6659ef5
Shahad Aziz <shahedalhadhrami@gmail.com>
2026-03-02 08:25:14 +0400

    feat: Configure custom Filament admin theme, add specific styling for one-time code input, and update Tailwind dependencies.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,8 @@ use App\Observers\CriterionObserver;
 use App\Observers\ProjectObserver;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Filament\Support\Facades\FilamentView;
+use Filament\View\PanelsRenderHook;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,9 +21,6 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Gate::before(function ($user, $ability) {
@@ -30,5 +29,24 @@ class AppServiceProvider extends ServiceProvider
 
         Criterion::observe(CriterionObserver::class);
         Project::observe(ProjectObserver::class);
+
+        FilamentView::registerRenderHook(
+            PanelsRenderHook::STYLES_AFTER,
+            fn(): string => '<style>
+                form:has(.fi-one-time-code-input-ctn) .fi-fo-field-label-col {
+                    display: flex;
+                    justify-content: center;
+                }
+                form:has(.fi-one-time-code-input-ctn) .fi-one-time-code-input-ctn {
+                    justify-content: center;
+                }
+                form:has(.fi-one-time-code-input-ctn) .fi-fo-field-content-col {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    text-align: center;
+                }
+            </style>'
+        );
     }
 }
